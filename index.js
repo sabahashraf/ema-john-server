6eaa17b4f471ed77113a9f1e984aca89d9a78c19
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-04-25 15:19:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("John is running and waiting for Ema");
+});
